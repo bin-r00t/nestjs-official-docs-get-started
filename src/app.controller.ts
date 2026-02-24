3e0eef4a1ee63ce498b16d1e85d3aaddf4b9ae59
liubin <bh0bama@sina.com>
2026-02-24 16:28:15 +0800

    feat: add note

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,9 +11,15 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('test')
+  @Get('log-response')
   testPureResponse(@Res({ passthrough: true }) response: Response): void {
-    console.log('testPureResponse called', response);
+    /**
+     * Note：当注入 response 对象时，相当于强迫 Nest 将这条 handler 进入了平台相关的模式（在这个例子中是 Express）。
+     * 并且需要导入对应的类型（在这个例子中是 express 的 Response 类型）。与此同时带来的好处可能是更大的灵活性，坏处是失去了平台无关的特性。
+     * 也就是说，使用了 @Res() 注入 response 对象的 handler 将无法在 Fastify 平台上运行。另外，手动注入 response 给我们 AI 应用
+     * 的流式数据返回提供了可能性，并且如果你没有手动调用 res.json() 或 res.send()，本轮请求会挂起
+     */
+    console.log('log-response called', response);
     response.setHeader('X-Custom-Header', 'CustomValue');
     response.status(200).send('ok!');
   }
